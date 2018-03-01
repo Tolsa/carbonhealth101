6bd36315c9f4c7bb2f8bf775e28cea3db1995b52
Nicolas Amiel <n.amiel.andersen@gmail.com>
2018-03-01 18:51:34 +0100

    adding doctors smart field to count how much are in a Practice

--- lib/forest_liana/collections/Practices.rb
+++ lib/forest_liana/collections/Practices.rb
@@ -1,16 +1,14 @@
 class Forest::Practice
   include ForestLiana::Collection
-
   collection :Practice
+
   action 'Schedule onboard call', fields: [{
     field: 'Date',
     type: 'Date',
   }]
   action 'Send doclist reminder'
 
-
-  field :doctors, type: 'Number' do
+  field :staff, type: 'Number' do
     object.doctors.count
   end
-
 end
